a40620e14fea6021daa5cc137cf8229b06c209e2
Madison Schirmer <90575585+m-schirmer@users.noreply.github.com>
2021-09-20 15:50:15 -0500

    second getting number from user and multiplying

---
+++ forms/adding/adding.js
@@ -0,0 +1,9 @@
+let num1 = 10
+let num2 - 20
+console.log(`The sum is ${num1 + num2}`)
+
+let newNum = Number(prompt("Enter a whole number: "))
+console.log(`If you multiply the second number with the new number you get ${num2 * newNum}`)
+
+let newNum2 = Number(prompt("Enter a whole number: "))
+console.log(`If you multiply the second number with the new number you get ${num2 * newNum2}`)
